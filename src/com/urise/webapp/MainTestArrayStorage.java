1d056bd777a443c6ce63f35a9f78bab0b47686df
Maksim-Bilous <max.biloys@gmail.com>
2025-04-15 18:54:09 +0300

    Lesson 3 fix ArrayStorage & SortedArr

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,7 +1,7 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.SortedArrayStorage;
 import com.urise.webapp.storage.Storage;
 
 
@@ -9,7 +9,7 @@ import com.urise.webapp.storage.Storage;
  * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final Storage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -19,13 +19,13 @@ public class MainTestArrayStorage {
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
-//        ARRAY_STORAGE.insert(r1, 0);
-//        ARRAY_STORAGE.insert(r2, 1);
-//        ARRAY_STORAGE.insert(r3, 2);
+        ARRAY_STORAGE.insert(r1, 0);
+        ARRAY_STORAGE.insert(r2, 1);
+        ARRAY_STORAGE.insert(r3, 2);
 
-        ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r2);
-        ARRAY_STORAGE.save(r3);
+//        ARRAY_STORAGE.save(r1);
+//        ARRAY_STORAGE.save(r2);
+//        ARRAY_STORAGE.save(r3);
 
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
@@ -34,17 +34,17 @@ public class MainTestArrayStorage {
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
-//        printAll();
-//        ARRAY_STORAGE.remove(r1.getUuid(), 0);
-//        printAll();
-//        ARRAY_STORAGE.clear();
-//        printAll();
-
         printAll();
-        ARRAY_STORAGE.delete(r1.getUuid());
+        ARRAY_STORAGE.remove(r1.getUuid(), 0);
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
+
+//        printAll();
+//        ARRAY_STORAGE.delete(r1.getUuid());
+//        printAll();
+//        ARRAY_STORAGE.clear();
+//        printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
     }
